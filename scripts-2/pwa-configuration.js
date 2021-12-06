b8d65c0044c7653a7dca04dbd4b450d4e13bfdff
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-12-06 20:19:06 +0100

    chore: vercel adapter for FunMiles

---
+++ scripts-2/pwa-configuration.js
@@ -0,0 +1,85 @@
+const scope = '/'
+
+const createRevision = () => {
+	let revision
+	return () => {
+		if (!revision) {
+			revision = `${Date.now()}`
+		}
+		return revision
+	}
+}
+
+const revision = createRevision()
+
+const pwaConfiguration = {
+	srcDir: './.vercel_build_output',
+	outDir: './.vercel_build_output/static/_app',
+	mode: 'development',
+	includeManifestIcons: false,
+	scope,
+	base: scope,
+	manifest: {
+		id: scope,
+		useCredentials: true,
+		short_name: 'FlightLog',
+		name: 'Flight Logging',
+		start_url: scope,
+		scope: scope,
+		display: 'standalone',
+		theme_color: "#3050f0",
+		background_color: "#ffffff",
+		icons: [
+			{
+				src: '/logo192.png',
+				sizes: "192x192",
+				type: "image/png"
+			},
+			{
+				src: '/logo512.png',
+				"sizes": "512x512",
+				"type": "image/png"
+			}
+		]
+	},
+	workbox: {
+		mode: 'development',
+		navigateFallback: scope,
+		globDirectory: './.vercel_build_output/',
+		globPatterns: ['robots.txt', '**/*.{js,css,html,ico,png,webp,svg,woff2,webmanifest}'],
+		globIgnores: [
+			'**/sw*', '**/workbox-*'
+		],
+		/** @type {import('workbox-build').ManifestTransform} */
+		manifestTransforms: [async(entries) => {
+			// map the icons and fonts on `./.vercel_build_output/static/
+			const resources = [
+				['static/robots.txt', '/robots.txt'],
+				['static/logo192.png', '/logo192.png'],
+				['static/logo512.png', '/logo512.png'],
+			]
+			const manifest = entries.map((e) => {
+				const { url } = e
+				const entry = resources.find(([entry]) => entry === url)
+				if (entry) {
+					return {
+						url: entry[1],
+						revision: e.revision
+					}
+				}
+				return e;
+			});
+
+			return { manifest };
+		}],
+		additionalManifestEntries: ['/', '/about', '/flights', '/circles', '/news'].map((url) => {
+			return {
+				url,
+				revision: revision()
+			}
+		})
+	}
+};
+
+
+export { pwaConfiguration }
